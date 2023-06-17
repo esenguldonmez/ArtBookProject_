20e29618ab604410098f1cba63751404fc7b2afb
esenguldonmez <esenguldonmez1@gmail.com>
2023-06-17 19:37:41 +0300

    New commit

---
+++ ArtBookProject/SecondController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondController.swift
+//  ArtBookProject
+//
+//  Created by Esengul Donmez on 17.06.2023.
+//
+
+import UIKit
+
+class SecondController: UIViewController {
+
+    
+    @IBOutlet weak var artImage: UIImageView!
+
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var artistText: UITextField!
+    
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+}
--- ArtBookProject/ViewController.swift
+++ ArtBookProject/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
     }
-
-
+    
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "secondToVC", sender: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        var cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "es"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    
 }
 
